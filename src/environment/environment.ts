d871627c7197e2993ae6d945cd64c118d7aa2cb8
Fabian <emmanuel.fabian.dev@gmail.com>
2025-06-29 20:00:32 +0100

    add dev base url

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   KOFI_URL: "https://ko-fi.com/D1D61G2KPD",
-  apiUrl: 'http://localhost:3000/api/v1'
+  apiUrl: 'https://appliq.onrender.com/api/v1'
 };
